dd250d18d645a7c968f3b661846108bbcba220f5
OliverTang <snakeystart@gmail.com>
2022-08-05 16:29:02 -0400

    update firebase utilities and try to debug with sign up new user bug

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,11 +1,13 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
+  signInWithRedirect,
   GoogleAuthProvider,
-} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+  createUserWithEmailAndPassword
+} from 'firebase/auth'
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
@@ -14,40 +16,51 @@ const firebaseConfig = {
   storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
   messagingSenderId: '626766232035',
   appId: '1:626766232035:web:506621582dab103a4d08d6',
-};
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
-const provider = new GoogleAuthProvider();
+const googleprovider = new GoogleAuthProvider()
 
-provider.setCustomParameters({
+googleprovider.setCustomParameters({
   prompt: 'select_account',
-});
+})
 
-export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleprovider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleprovider)
 
-export const db = getFirestore();
+export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-  const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}) => {
+  if (!userAuth) return
 
-  const userSnapshot = await getDoc(userDocRef);
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  const userSnapshot = await getDoc(userDocRef)
 
   if (!userSnapshot.exists()) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
 
     try {
       await setDoc(userDocRef, {
         displayName,
         email,
         createdAt,
-      });
+        ...additionalInformation
+      })
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log('error creating the user', error.message)
     }
   }
 
-  return userDocRef;
-};
+  return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
