bd31eeb8787ae2eaf75642f58fd26f1217953f5a
OliverTang <snakeystart@gmail.com>
2022-08-06 16:04:44 -0400

    finishing on product-card and including the shop component for specific products and to begin with cart component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component';
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+import Authentication from './routes/authentication/authentication.component'
+import Shop from "./routes/shop/shop.component"
 
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
 
 const App = () => {
   return (
@@ -17,7 +15,7 @@ const App = () => {
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
