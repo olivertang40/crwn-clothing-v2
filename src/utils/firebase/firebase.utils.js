62acfdd4f953bad502d168a09c9f6653721fd7b1
OliverTang <snakeystart@gmail.com>
2022-08-05 20:52:14 -0400

    created sign up component, the form input component, button component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,12 +10,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAxpvVjLxH9cByHyDcn-913czeSVQQAfVo",
+  authDomain: "crwn-clothing-db-63194.firebaseapp.com",
+  projectId: "crwn-clothing-db-63194",
+  storageBucket: "crwn-clothing-db-63194.appspot.com",
+  messagingSenderId: "116117919310",
+  appId: "1:116117919310:web:a380a659e5565c0ffcd51f"
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
