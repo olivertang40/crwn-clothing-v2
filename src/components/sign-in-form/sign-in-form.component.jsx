701f1a2a3ecf2bc0d1225f51454cd41aa9bf6542
OliverTang <snakeystart@gmail.com>
2022-08-06 03:43:23 -0400

    add in the user context and sign out component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,63 +1,67 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react'
 
-import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+
+import { UserContext } from "../../contexts/user.context"
 
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
-} from '../../utils/firebase/firebase.utils';
+} from '../../utils/firebase/firebase.utils'
 
-import './sign-in-form.styles.scss';
+import './sign-in-form.styles.scss'
 
 const defaultFormFields = {
   email: '',
   password: '',
-};
+}
 
 const SignInForm = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { email, password } = formFields;
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { email, password } = formFields
+
+  const { setCurrentUser } = useContext(UserContext)
 
   const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
+    setFormFields(defaultFormFields)
+  }
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
+    const { user } = await signInWithGooglePopup()
+    await createUserDocumentFromAuth(user)
+  }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
+      const { user } = await signInAuthUserWithEmailAndPassword(
         email,
         password
-      );
-      console.log(response);
-      resetFormFields();
+      )
+
+      resetFormFields()
     } catch (error) {
       switch (error.code) {
         case 'auth/wrong-password':
-          alert('incorrect password for email');
-          break;
+          alert('incorrect password for email')
+          break
         case 'auth/user-not-found':
-          alert('no user associated with this email');
-          break;
+          alert('no user associated with this email')
+          break
         default:
-          console.log(error);
+          console.log(error)
       }
     }
-  };
+  }
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value } = event.target
 
-    setFormFields({ ...formFields, [name]: value });
-  };
+    setFormFields({ ...formFields, [name]: value })
+  }
 
   return (
     <div className='sign-up-container'>
@@ -89,7 +93,7 @@ const SignInForm = () => {
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default SignInForm;
+export default SignInForm
