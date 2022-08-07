cd019cc577c009c63a70abe655e1612a0eddc1e3
OliverTang <snakeystart@gmail.com>
2022-08-07 19:20:14 -0400

    pulling data from firebase database using getDoc

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,24 +1,30 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from 'react'
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
-
-import SHOP_DATA from '../shop-data.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils'
 
 export const ProductsContext = createContext({
   products: [],
-});
+})
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments()
+      console.log(categoryMap)
+    }
+    getCategoriesMap()
+  }, [])
 
   // useEffect(() => {
-  //   addCollectionAndDocuments('collections', SHOP_DATA);
-  // }, []);
+  //   addCollectionAndDocuments('categories', SHOP_DATA)
+  // }, [])
 
-  const value = { products };
+  const value = { products }
   return (
     <ProductsContext.Provider value={value}>
       {children}
     </ProductsContext.Provider>
-  );
-};
+  )
+}
