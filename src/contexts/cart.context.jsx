8a6c9529df1ef626b1d2dd4b96c346090bedcca8
OliverTang <snakeystart@gmail.com>
2022-08-07 03:23:07 -0400

    added in our addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,36 +1,45 @@
-import { createContext, useState } from 'react';
+import { toContainHTML } from '@testing-library/jest-dom/dist/matchers'
+import { createContext, useState, useEffect } from 'react'
 
 export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
-  );
+  )
 
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === productToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
-    );
+    )
   }
 
-  return [...cartItems, { ...productToAdd, quantity: 1 }];
-};
+  return [...cartItems, { ...productToAdd, quantity: 1 }]
+}
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsOpen: () => {},
+  setIsOpen: () => { },
   cartItems: [],
-  addItemToCart: () => {},
-});
+  addItemToCart: () => { },
+  cartCount: 0
+
+})
 
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([])
+  const [cartCount, setCartCount] = useState(0)
 
   const addItemToCart = (product) =>
-    setCartItems(addCartItem(cartItems, product));
+    setCartItems(addCartItem(cartItems, product))
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItems])
 
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
-};
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount }
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>
+}
